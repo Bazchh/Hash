0879b24af9f992d274e990a9d6609d1f049aa865
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2022-11-01 15:45:37 -0300

    Add files via upload

---
+++ hash/main.h
@@ -0,0 +1,18 @@
+#include "main.cpp"
+
+struct cidade;
+struct gps;
+struct dataItem;
+dataItem *getItens(cidade *, gps *);
+void printDataItens(dataItem *);
+void saveDataItens(dataItem *);
+cidade *getCidades(char *arquivo);
+gps *getGps(char *);
+char* remover(char* text);
+void init(hash &H);
+void printHash(hash H);
+int inserir(hash H, dataItem d);
+int remover(hash H, dataItem *d, int id);
+dataItem *buscar(hash H, dataItem *e,int id);
+int hash_modular(dataItem *d,int k);
+int TabelaHash();;
